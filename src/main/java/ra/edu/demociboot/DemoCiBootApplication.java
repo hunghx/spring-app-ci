321e5cae8c690e2c36635a42bb23d3396ea1933f
hunghx <baby10051999@gmail.com>
2026-08-25 20:29:24 +0700

    v1

---
+++ src/main/java/ra/edu/demociboot/DemoCiBootApplication.java
@@ -0,0 +1,13 @@
+package ra.edu.demociboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCiBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCiBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ra/edu/demociboot/DemoCiBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.edu.demociboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
